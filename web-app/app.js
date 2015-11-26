dbfbbf286cde111e2b25c5c2856af1bc7562a03a
Patrick Rainier R. Juen <patrickrainier@gmail.com>
2015-11-26 15:33:27 +0800

    non state modal

--- web-app/app.js
+++ web-app/app.js
@@ -1,18 +1,33 @@
-var app = angular.module('restprac', ['ngResource', 'ui.router'])
+var app = angular.module('restprac', ['ngResource', 'ui.router', 'ui.bootstrap'])
 
 app.config(['$stateProvider', '$locationProvider', function($stateProvider, $locationProvider){
     $locationProvider.html5Mode(true);
-    $stateProvider.state("books", {
-        url: "/books",
-        templateUrl: "template.html",
-        resolve: {
-            Book: 'Book',
-            books: ['Book', function(Book){
-                return Book.query().$promise
-            }]
-        },
-        controller: 'BookController'
-    })
+    $stateProvider
+        .state("books", {
+            url: "/books",
+            templateUrl: "template.html",
+            resolve: {
+                Book: 'Book',
+                books: ['Book', function(Book){
+                    return Book.query().$promise
+                }]
+            },
+            controller: 'BookController'
+         })
+        .state("modal", {
+            url: "/modal",
+            onEnter: function($stateParams, $state, $uibModal){
+                $uibModal.open({
+                    templateUrl: "modal.html",
+                    controller: "ModalController"
+                }).result.then(function(result){
+                        if(result){
+                            return $state.transitionTo("books")
+                        }
+                    })
+            }
+
+        })
 }])
 
 
@@ -31,6 +46,34 @@ app.controller('BookController', ['$scope', 'books',function($scope, books){
 
     console.log($scope.books.length)
 
+}])
 
+app.controller('ModalController', ['$scope', '$uibModal', function($scope, $uibModal){
+    //$scope.dismiss = function(){
+    //    $scope.dismiss()
+    //}
 
+    $scope.temp = "walaw"
+    $scope.open = function(){
+        console.log("nag sulod")
+        var modalInstance = $uibModal.open({
+            animation: true,
+            templateUrl: "modal.html",
+            controller: "ModalInstanceController"
+        })
+
+        modalInstance.result.then(function () {
+            $scope.temp = "temp variable"
+        })
+    }
+}])
+
+app.controller('ModalInstanceController', ['$scope', '$uibModalInstance', function($scope, $uibModalInstance){
+    $scope.ok = function(){
+        $uibModalInstance.close()
+    }
+
+    $scope.cancel = function(){
+        $uibModalInstance.dismiss('cancel')
+    }
 }])
